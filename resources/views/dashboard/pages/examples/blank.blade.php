973020a97408262d6e145f2226f6fb9aac0e1c4f
hackluv <hackluv@hackluvs-MacBook-Pro.local>
2018-08-10 16:48:37 +0700

    remove old dashboard - add MD-P

--- resources/views/dashboard/pages/examples/blank.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('dashboard.layouts.master')
-
-@section('content')
-    <div class="container-fluid">
-        <div class="block-header">
-            <h2>BLANK PAGE</h2>
-        </div>
-    </div>
-@endsection
