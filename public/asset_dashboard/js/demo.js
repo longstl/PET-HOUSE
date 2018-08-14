3ef32a69f04c8d6dec802ea487df1b752a655ced
longstl <jonnykhanh1102@gmail.com>
2018-08-14 15:38:59 +0700

    remove auto open show dropdown

--- public/asset_dashboard/js/demo.js
+++ public/asset_dashboard/js/demo.js
@@ -12,7 +12,7 @@ $().ready(function(){
 
     if( window_width > 767 && fixed_plugin_open == 'Dashboard' ){
         if($('.fixed-plugin .dropdown').hasClass('show-dropdown')){
-            $('.fixed-plugin .dropdown').addClass('open');
+            // $('.fixed-plugin .dropdown').addClass('open');
         }
 
     }
