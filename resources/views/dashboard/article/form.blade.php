5387eee0212dd6951f6f0980acd118a3f6fd25c5
trankhanh1999 <khanhtnd00540@fpt.edu.vn>
2018-08-19 23:23:59 +0700

    Fix lỗi validate và delete sản phẩm phần article controller

--- resources/views/dashboard/article/form.blade.php
+++ resources/views/dashboard/article/form.blade.php
@@ -11,33 +11,42 @@
                 </div>
                 <div class="card-content">
                     <h4 class="card-title">ADD ARTICLE</h4>
+                    @if ($errors->any())
+                        <div class="alert alert-rose">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <form method="post" action="/dashboard/article" class="form-horizontal">
                         {{csrf_field()}}
                         <div class="card-content">
                             <div class="row">
                                 <label class="col-sm-2 label-on-left">Title</label>
                                 <div class="col-sm-4">
-                                    <div class="form-group label-floating is-empty">
+                                    <div class="form-group label-floating is-empty{{$errors->has('title')?' has-error':''}}">
                                         <label class="control-label"></label>
-                                        <input type="text" name="title" class="form-control">
+                                        <input type="text" name="title" class="form-control{{$errors->has('title')?' error':''}}">
                                         <span class="material-input"></span></div>
                                 </div>
                             </div>
                             <div class="row">
                                 <label class="col-sm-2 label-on-left">Content</label>
                                 <div class="col-sm-8">
-                                    <div class="form-group label-floating is-empty">
+                                    <div class="form-group label-floating is-empty{{$errors->has('content')?' has-error':''}}">
                                         <label class="control-label"></label>
-                                        <textarea type="text" name="content" class="form-control"></textarea>
+                                        <input type="text" name="content" class="form-control{{$errors->has('content')?' error':''}}">
                                         <span class="material-input"></span></div>
                                 </div>
                             </div>
                             <div class="row">
                                 <label class="col-sm-2 label-on-left">Images</label>
                                 <div class="col-sm-8">
-                                    <div class="form-group label-floating is-empty">
+                                    <div class="form-group label-floating is-empty{{$errors->has('images')?' has-error':''}}">
                                         <label class="control-label"></label>
-                                        <input type="text" name="images" class="form-control">
+                                        <input type="text" name="images" class="form-control{{$errors->has('images')?' error':''}}">
                                         <span class="material-input"></span></div>
                                 </div>
                             </div>
