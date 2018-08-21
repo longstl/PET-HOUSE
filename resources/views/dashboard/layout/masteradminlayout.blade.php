147b00f12a87c4ed469d402cd8b9d45ff148bf87
thuonghvd0056 <thuonghvd00564@fpt.edu.vn>
2018-08-21 17:06:16 +0700

    Fix logout admin

--- resources/views/dashboard/layout/masteradminlayout.blade.php
+++ resources/views/dashboard/layout/masteradminlayout.blade.php
@@ -59,7 +59,16 @@
                                 <a href="#">Settings</a>
                             </li>
                             <li>
-                                <a href="#">LogOut</a>
+                                {{--<a href="#">LogOut</a>--}}
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
                             </li>
                         </ul>
                     </div>
