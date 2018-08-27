1942a9fe8c1b2191506634e8d02b9362aa15e9dc
thao <thaottd00576@fpt.edu.vn>
2018-08-27 10:01:34 +0700

    Add chart

--- app/ShoppingCart.php
+++ app/ShoppingCart.php
@@ -15,6 +15,15 @@ class ShoppingCart
     /**
      * ShoppingCart constructor.
      */
+
+    public static function getCart()
+    {
+        if (Session::has('cart')) {
+            return Session::get('cart');
+        }
+        return new ShoppingCart();
+    }
+
     function __construct()
     {
         $this->items = array();
@@ -31,4 +40,25 @@ class ShoppingCart
     function getTotalMoneyWithFormat(){
         return sprintf('%s', number_format($this->getTotalMoney(), 0));
     }
+
+    public static function getTotalItem()
+    {
+        $count = 0;
+        if (Session::has('cart')) {
+            $shopping_cart = Session::get('cart');
+            $count = self::calculateTotalItem($shopping_cart);
+        }
+        return $count;
+    }
+
+    public static function calculateTotalItem($shopping_cart)
+    {
+        $count = 0;
+        foreach ($shopping_cart->items as $item) {
+            $count += $item->quantity;
+        }
+        return $count;
+    }
+
+
 }
