7fa6f9c6aaecf2f4f7b9339e3c9d466f77a960cc
thao <thaottd00576@fpt.edu.vn>
2018-09-12 21:47:03 +0700

    Add Search for cat (js)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,12 +18,11 @@
           integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw"
           crossorigin="anonymous">
     <link rel="stylesheet" href="/css/cursor.css" type="text/css">
+    <script src="/js/jquery-3.3.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
             integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
             crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
-            crossorigin="anonymous"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
             integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
             crossorigin="anonymous"></script>
@@ -41,5 +40,6 @@
 @include('partials.footer')
 
 @yield('scripts')
+@yield('extra-js')
 </body>
 </html>
--- resources/views/pethouse/cat.blade.php
+++ resources/views/pethouse/cat.blade.php
@@ -1,19 +1,7 @@
 @extends('layouts.master')
 @section('title')
 @endsection
-@section('scripts')
-    <script src="/js/modal-product.js" type="text/javascript"/>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-            crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-            crossorigin="anonymous"></script>
 
-@endsection
 @section('content')
 
     <link rel="stylesheet" href="/css/accessories-product.css" type="text/css">    <br>
@@ -75,3 +63,22 @@
             {{$product->links()}}</div>
     </div>
 @endsection
+@section('extra-js')
+    <script>
+        function changeFunc() {
+            var selectBox = document.getElementById("search-price");
+            var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+            var price = selectedValue.split('-');
+            $.ajax({
+                'url':'/search-price/cat'+'?min='+price[0]+'&max='+price[1],
+                'method':'GET',
+                success: function (res) {
+                    console.log(res);
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+            });
+        }
+    </script>
+    @endsection
