1273aea1b7a4e2fbf3d2b3504c40535988bff9c8
thao <thaottd00576@fpt.edu.vn>
2018-09-13 23:19:16 +0700

    fix Chart

--- resources/views/dashboard/charts/chart.blade.php
+++ resources/views/dashboard/charts/chart.blade.php
@@ -4,7 +4,8 @@
     'current_sub_menu'=>'chart'
 ])
 @section('content')
-    <div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
+    <div id="reportrange"
+         style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
         <i class="fa fa-calendar"></i>&nbsp;
         <span></span> <i class="fa fa-caret-down"></i>
     </div>
@@ -24,16 +25,16 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
     {{--<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>--}}
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css"/>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-        $(function() {
+        $(function () {
             var start = moment().subtract(29, 'days');
             var end = moment();
 
             function cb(start, end) {
                 $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-                loadChart(start,end);
+                loadChart(start, end);
             }
 
             $('#reportrange').daterangepicker({
@@ -49,44 +50,49 @@
 
             cb(start, end);
 
-        google.charts.load('current', {'packages': ['line']});
-        google.charts.setOnLoadCallback(function () {
-                loadChart(start,end);
-        });
-        function loadChart(start,end) {
-            // alert('/api-get-chart-data?from='+start.format('YYYY-MM-DD')+'&to='+end.format('YYYY-MM-DD'));
-            $.ajax({
-                url: '/api-get-chart-data?from='+start.format('YYYY-MM-DD')+'&to='+end.format('YYYY-MM-DD'),
-                method: 'GET',
-                success: function (resp) {
-                    drawChart(resp);
-                },
-                error: function () {
-                    swal('Have Error', 'Do not call API', 'error');
-                }
+            google.charts.load('current', {'packages': ['line']});
+            google.charts.setOnLoadCallback(function () {
+                loadChart(start, end);
             });
-        }
 
-        function drawChart(chart_data) {
-            var data = new google.visualization.DataTable();
-            data.addColumn('date', 'Date');
-            data.addColumn('number', 'Revenue');
-            for (var i = 0; i < chart_data.length; i++) {
-                data.addRow([new Date(chart_data[i].day), Number(chart_data[i].revenue)]);
+            function loadChart(start, end) {
+                // alert('/api-get-chart-data?from='+start.format('YYYY-MM-DD')+'&to='+end.format('YYYY-MM-DD'));
+                $.ajax({
+                    url: '/api-get-chart-data?from=' + start.format('YYYY-MM-DD') + '&to=' + end.format('YYYY-MM-DD'),
+                    method: 'GET',
+                    success: function (resp) {
+                        if (resp.length == 0) {
+                            swal('Do not get Data', 'Please try again later.', 'warning');
+                            return;
+                        }
+                        drawChart(resp);
+                    },
+                    error: function () {
+                        swal('Have Error', 'Do not call API', 'error');
+                    }
+                });
             }
-            var options = {
-                chart: {
-                    title: 'Revenue chart over time',
-                    subtitle: '(USD)'
-                },
-                height: 500,
-                hAxis: {
-                    format: 'MM/dd/yyyy'
+
+            function drawChart(chart_data) {
+                var data = new google.visualization.DataTable();
+                data.addColumn('date', 'Date');
+                data.addColumn('number', 'Revenue');
+                for (var i = 0; i < chart_data.length; i++) {
+                    data.addRow([new Date(chart_data[i].day), Number(chart_data[i].revenue)]);
                 }
-            };
-            var chart = new google.charts.Line(document.getElementById('linechart_material'));
-            chart.draw(data, google.charts.Line.convertOptions(options));
-        }
+                var options = {
+                    chart: {
+                        title: 'Revenue chart over time',
+                        subtitle: '(USD)'
+                    },
+                    height: 500,
+                    hAxis: {
+                        format: 'MM/dd/yyyy'
+                    }
+                };
+                var chart = new google.charts.Line(document.getElementById('linechart_material'));
+                chart.draw(data, google.charts.Line.convertOptions(options));
+            }
         });
 
     </script>
